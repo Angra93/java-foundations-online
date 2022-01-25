012af8869a62f5d6da40b991aef2c21db8387b68
Angra93 <angraandreev@gmail.com>
2022-01-25 03:05:14 +0300

    1

---
+++ src/main/java/ru/itsjava/object/Dog.java
@@ -0,0 +1,31 @@
+package ru.itsjava.object;
+
+public class Dog {
+    private final String nickname;
+    private int pawsCount;
+
+    public Dog(String nickname, int pawsCount) {
+        this.nickname = nickname;
+        this.pawsCount = pawsCount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        // если ссылка на 1 и тот же объект то true, заканчивается проверка
+        if (this == o) return true;
+        // если объект равен null или другому классу то false
+        if (o == null || getClass() != o.getClass()) return false;
+//     Класс Dog работает с объктом Dog
+        Dog dog = (Dog) o;
+//     проверяется количечство лап, если не совпадает то false
+        if (pawsCount != dog.pawsCount) return false;
+        // Сравнивается имена
+        return nickname.equals(dog.nickname);
+    }
+
+    @Override
+    public int hashCode() {
+        // возвращает хэшкод имени
+        return nickname.hashCode();
+    }
+}
--- src/main/java/ru/itsjava/oop/Telephone.java
+++ src/main/java/ru/itsjava/oop/Telephone.java
@@ -9,11 +9,35 @@ public class Telephone {
     }
 
     public Telephone(String brand, boolean isHomey) {
+        // В equals сравнивается brand с brand
         this.brand = brand;
+        // В equals сравнивается isHomey с isHomey
         this.isHomey = isHomey;
     }
 
     public String toString() {
         return "{ brand:" + brand + " isHomey:" + isHomey + "}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        // если ссылка на 1 и тот же объект то true, заканчивается проверка
+        if (this == o) return true;
+        // если объект равен null или другому классу то false
+        if (o == null || getClass() != o.getClass()) return false;
+
+        // Класс Telephone работает с объктом Telephone
+        Telephone telephone = (Telephone) o;
+
+        // Проверяется isHomey если не равны то false
+        if (isHomey != telephone.isHomey) return false;
+        // В ином случае возвращается и сравнивает brand
+        return brand.equals(telephone.brand);
+    }
+
+    @Override
+    public int hashCode() {
+        // возвращает хэшкод brand
+        return brand.hashCode();
     }
 }
